04f2697240c5d68696470a72dd7cef488a2d6ec2
karo.gcode@gmail.com <karo.gcode@gmail.com@6c993605-5ea2-5009-e547-d362a36fed53>
2012-01-04 06:25:52 +0000

    Nueva interface para la comunicación entre módulos (servicio de I/O).

--- trunk/Cirse/Graphics/src/org/itver/graphics/io/LoadingService.java
+++ trunk/Cirse/Graphics/src/org/itver/graphics/io/LoadingService.java
@@ -5,7 +5,7 @@
 package org.itver.graphics.io;
 
 import java.io.File;
-import javax.media.j3d.BranchGroup;
+import org.itver.graphics.model.MainSceneComponent;
 import org.itver.graphics.util.ComponentType;
 
 /**
@@ -16,6 +16,6 @@ public interface LoadingService {
     
     public boolean isLoadable(ComponentType type);
     
-    public BranchGroup getArm(File src);
+    public MainSceneComponent getArm(File src);
     
 }
